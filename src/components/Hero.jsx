81cabd47be746dbcded294a3def16eda7fe388cb
Starklord17 <maxi.sm117@gmail.com>
2025-01-01 13:42:46 -0300

    :art: Integrate GSAP for enhanced video transitions in Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,6 +2,8 @@ import { useRef } from "react";
 import { useState } from "react";
 import Button from "./Button";
 import { TiLocationArrow } from "react-icons/ti";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 const Hero = () => {
   const [currentIndex, setCurrentIndex] = useState(1);
@@ -14,7 +16,7 @@ const Hero = () => {
 
   const handleVideoLoad = () => {
     setLoadedVideos((prev) => prev + 1);
-  }
+  };
 
   const upcomingVideoIndex = (currentIndex % totalVideos) + 1;
 
@@ -23,6 +25,30 @@ const Hero = () => {
 
     setCurrentIndex(upcomingVideoIndex);
   };
+
+  useGSAP(() => {
+    if(hasClicked) {
+      gsap.set("#next-video", {visibility: 'visible'});
+
+      gsap.to('#next-video', {
+        transformOrigin: 'center',
+        scale: 1,
+        width: '100%',
+        height: '100%',
+        duration: 1,
+        ease: 'power1.inOut',
+        onStart: () => nextVideoRef.current.play(),
+      })
+
+      gsap.from('#current-video', {
+        transformOrigin: 'center center',
+        scale: 0,
+        duration: 1.5,
+        ease: 'power1.inOut',
+      })
+    }
+
+  }, {dependencies: [currentIndex], revertOnUpdate: true})
 
   const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
 
@@ -34,7 +60,10 @@ const Hero = () => {
       >
         <div>
           <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
-            <div onClick={handleMiniVdClick} className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100">
+            <div
+              onClick={handleMiniVdClick}
+              className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100"
+            >
               <video
                 ref={nextVideoRef}
                 src={getVideoSrc(upcomingVideoIndex)}
@@ -47,18 +76,20 @@ const Hero = () => {
             </div>
           </div>
 
-          <video 
+          <video
             ref={nextVideoRef}
             src={getVideoSrc(currentIndex)}
             loop
             muted
-            id='next-video'
+            id="next-video"
             className="absolute-center invisible absolute z-20 size-64 object-cover object-center"
             onLoadedData={handleVideoLoad}
           />
 
-          <video 
-            src={getVideoSrc(currentIndex === totalVideos - 1 ? 1 : currentIndex)}
+          <video
+            src={getVideoSrc(
+              currentIndex === totalVideos - 1 ? 1 : currentIndex
+            )}
             // autoPlay
             loop
             muted
@@ -72,15 +103,27 @@ const Hero = () => {
 
           <div className="absolute left-0 top-0 z-40 size-full">
             <div className="mt-24 px-5 sm:px-10">
-              <h1 className="special-font hero-heading text-blue-100">Redefi<b>n</b>e</h1>
+              <h1 className="special-font hero-heading text-blue-100">
+                Redefi<b>n</b>e
+              </h1>
 
-              <p className="mb-5 max-w-64 font-robert-regular text-blue-100">Enter the Metagame Layer <br /> Unleash the Play Economy</p>
+              <p className="mb-5 max-w-64 font-robert-regular text-blue-100">
+                Enter the Metagame Layer <br /> Unleash the Play Economy
+              </p>
 
-              <Button id='watch-trailer' title='Watch Trailer' leftIcon={<TiLocationArrow />} containerClass='!bg-yellow-300 flex-center gap-1' />
+              <Button
+                id="watch-trailer"
+                title="Watch Trailer"
+                leftIcon={<TiLocationArrow />}
+                containerClass="!bg-yellow-300 flex-center gap-1"
+              />
             </div>
           </div>
-
         </div>
+
+        <h1 className="special-font hero-heading absolute bottom-5 right-5 text-black">
+          G<b>a</b>ming
+        </h1>
       </div>
     </div>
   );
