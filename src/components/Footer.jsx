2c8a568849aefcfed1034a029b8b7cc357d37057
Starklord17 <maxi.sm117@gmail.com>
2025-01-02 01:20:05 -0300

    :art: Add Footer component and include Contact section in App

---
+++ src/components/Footer.jsx
@@ -0,0 +1,44 @@
+import { FaTwitter, FaMedium, FaGithub } from "react-icons/fa";
+import { SiAlby } from "react-icons/si";
+
+const socialLinks = [
+  { href: "https://getalby.com/p/maxsm", icon: <SiAlby /> },
+  { href: "https://x.com/maxsm117", icon: <FaTwitter /> },
+  { href: "https://github.com/Starklord17", icon: <FaGithub /> },
+  { href: "https://medium.com", icon: <FaMedium /> },
+];
+
+const Footer = () => {
+  return (
+    <footer className="w-screen bg-[#5542ff] py-4 text-black">
+      <div className="container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row">
+        <p className="text-center text-sm font-light md:text-left">
+          ©Nova 2024. All rights reserved
+        </p>
+
+        <div className="flex justify-center gap-4  md:justify-start">
+          {socialLinks.map((link, index) => (
+            <a
+              key={index}
+              href={link.href}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-black transition-colors duration-500 ease-in-out hover:text-white"
+            >
+              {link.icon}
+            </a>
+          ))}
+        </div>
+
+        <a
+          href="#privacy-policy"
+          className="text-center text-sm font-light hover:underline md:text-right"
+        >
+          Privacy Policy
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
