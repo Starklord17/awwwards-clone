48a71372b47294064e19bd9096009cd131000725
Starklord17 <maxi.sm117@gmail.com>
2025-01-05 15:30:09 -0300

    :zap: Add id attribute to BentoCard component for improved accessibility

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -80,7 +80,7 @@ export const BentoCard = ({ src, title, description, isComingSoon }) => {
   const handleMouseLeave = () => setHoverOpacity(0);
 
   return (
-    <div className="relative size-full">
+    <div id="metagame" className="relative size-full">
       <video
         src={src}
         loop
